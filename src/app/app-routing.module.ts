45b965466e68f66c7af76022d02c2711a2cf3d3d
Emmanuel Correa <ebcorreac@gmail.com>
2020-07-14 00:28:20 -0400

    Primer Commit de Angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { GotyComponent } from './pages/goty/goty.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'inicio', component: HomeComponent},
+  {path:'goty',   component: GotyComponent},
+  //This enabled everythings links not found 
+  {path:'**',     pathMatch:'full',redirectTo:'inicio'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
